1ea6005bc799f7c0900ac9c3fd0edb54588178aa
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-17 10:05:27 -0800

    d

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnlpYazaSFHVyWYy3yyEzo47TRCB5IcMw",
+    authDomain: "pioneerhack-e9ede.firebaseapp.com",
+    databaseURL: "https://pioneerhack-e9ede-default-rtdb.firebaseio.com",
+    projectId: "pioneerhack-e9ede",
+    storageBucket: "pioneerhack-e9ede.appspot.com",
+    messagingSenderId: "1006929338819",
+    appId: "1:1006929338819:web:3e82bb46f56ea558f5dd76"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
