2455786aa604f26411143bf1570123fc53d0807c
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-17 14:07:32 -0800

    s

--- home.js
+++ home.js
@@ -1,9 +1,11 @@
 function openNav(){
     document.getElementById("sidebar").style.width="250px";
+    document.getElementById("openSidebar").style.display="none";
 }
 
 function closeNav(){
     document.getElementById("sidebar").style.width="0";
+    document.getElementById("openSidebar").style.display="flex";
 }
 time = document.getElementById("time")
 now = new Date().getHours();
