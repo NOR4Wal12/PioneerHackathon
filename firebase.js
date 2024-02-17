1cc223dc3e890d0a6dee660e5ecf56b8f30bf869
fyrebolt <82546290+fyrebolt@users.noreply.github.com>
2024-02-17 14:06:43 -0800

    Update firebase.js
    multiple

--- firebase.js
+++ firebase.js
@@ -30,5 +30,23 @@ function pushStretchR(id){
     email = localStorage.getItem("user")
     user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
     user = user.substring(0,user.indexOf("@"))
-    database.ref(user + '/' + id).set(r2).then((snapshot)=>{})
+    if (id === 1){
+        database.ref(user + '/stetches').set({1:r2}).then((snapshot)=>{})
+    } else if (id == 2){
+        database.ref(user + '/stetches').set({2:r2}).then((snapshot)=>{})
+    } else if (id == 3){
+        database.ref(user + '/stetches').set({3:r2}).then((snapshot)=>{})
+    } else if (id == 4){
+        database.ref(user + '/stetches').set({4:r2}).then((snapshot)=>{})
+    } else if (id == 5){
+        database.ref(user + '/stetches').set({5:r2}).then((snapshot)=>{})
+    } else if (id == 6){
+        database.ref(user + '/stetches').set({6:r2}).then((snapshot)=>{})
+    } else if (id == 7){
+        database.ref(user + '/stetches').set({7:r2}).then((snapshot)=>{})
+    } else if (id == 8){
+        database.ref(user + '/stetches').set({8:r2}).then((snapshot)=>{})
+    } else if (id == 9){
+        database.ref(user + '/stetches').set({9:r2}).then((snapshot)=>{})
+    }
 }
