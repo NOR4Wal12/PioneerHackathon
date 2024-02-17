b4eb7d01627ea0d352590c19feb719465767741d
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-17 14:02:49 -0800

    u

--- home.js
+++ home.js
@@ -5,14 +5,14 @@ function openNav(){
 function closeNav(){
     document.getElementById("sidebar").style.width="0";
 }
-
+time = document.getElementById("time")
 now = new Date().getHours();
 if (now >= 0 && now <= 11){
-
+    time.innerHTML = "morning"
 }
 else if (now >=12 && now <= 18){
-
+    time.innerHTML = "afternoon"
 }
 else {
-
+    time.innerHTML = "evening"
 }
