ca919b63ed10d40e59b0b8957bcf085ed699a164
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2024-02-17 11:32:08 -0800

    nose attmept 2

--- vision.js
+++ vision.js
@@ -10,7 +10,8 @@ function setup() {
     poseNet.on(pose, gotPoses)
   }
 
-function gotPoses(){
+function gotPoses(poses){
+    console.log(poses)
     if (postMessage.length > 0){
         pose = poses[0].pose;
 
@@ -27,17 +28,19 @@ const button = document.getElementById("changeButton")
   button.onclick = (event) => {
     event.preventDefault()
     cameraOn *= -1
-    console.log("pressed")
     window.alert("pressed")
   } 
 
   function draw() {
     if (cameraOn == 1){
         background(220);
-        image(video, 0, 0, 64)
+        image(video, 0, 0)
         if (pose) {
             fill(255,0,0)
-            ellipse(pose.nose.x, pose.nose.y)
+            ellipse(pose.nose.x, pose.nose.y, 64)
+        } else{
+            fill(0,255,0)
+            ellipse(340, 240, 64)
         }
     } else{
         background(220);
