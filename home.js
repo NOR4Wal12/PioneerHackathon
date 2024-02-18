d0389145e8ec90af6c006d9bb0f8d0f5dc8ea780
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-18 00:17:13 -0800

    s

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 button = document.getElementById("openSidebar")
 function openNav(){
-    document.getElementById("sidebar").style.width="300px";
+    document.getElementById("sidebar").style.width="350px";
     button.onclick = closeNav
 }
 
