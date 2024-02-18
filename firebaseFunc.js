a6860898ad7ec85d7944b5cc6ea248e4e73a6223
fyrebolt <82546290+fyrebolt@users.noreply.github.com>
2024-02-17 18:41:37 -0800

    Update firebaseFunc.js
    z

--- firebaseFunc.js
+++ firebaseFunc.js
@@ -8,20 +8,10 @@ function closeNav(){
     document.getElementById("sidebar").style.width="0";
     button.onclick = openNav
 }
-time = document.getElementById("time")
-nameBox = document.getElementById("name")
+
+nameBox = document.getElementById("profileName")
 name = localStorage.getItem("name")
 nameBox.innerHTML = name
-now = new Date().getHours();
-if (now >= 0 && now <= 11){
-    time.innerHTML = "morning"
-}
-else if (now >=12 && now <= 18){
-    time.innerHTML = "afternoon"
-}
-else {
-    time.innerHTML = "evening"
-}
 
 function pushStretchR(id){
     r2 = document.getElementById("testFirebaseInput").value
