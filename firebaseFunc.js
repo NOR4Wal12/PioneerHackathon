d1a06759c1f575e4d971bb93b6c7c18be64e8186
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-17 18:43:33 -0800

    s

--- firebaseFunc.js
+++ firebaseFunc.js
@@ -1,17 +1,4 @@
-button = document.getElementById("openSidebar")
-function openNav(){
-    document.getElementById("sidebar").style.width="300px";
-    button.onclick = closeNav
-}
 
-function closeNav(){
-    document.getElementById("sidebar").style.width="0";
-    button.onclick = openNav
-}
-
-nameBox = document.getElementById("profileName")
-name = localStorage.getItem("name")
-nameBox.innerHTML = name
 
 function pushStretchR(id){
     r2 = document.getElementById("testFirebaseInput").value
