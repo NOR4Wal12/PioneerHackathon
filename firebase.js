abb825d51739d63d7a52b47a63991fdf1e4c635e
fyrebolt <82546290+fyrebolt@users.noreply.github.com>
2024-02-18 02:05:23 -0800

    Update firebase.js
    session storage for custom workout

--- firebase.js
+++ firebase.js
@@ -16,6 +16,19 @@ function check(){
   if(localStorage.getItem("loggedIn")!="yes"){
       window.location.href = "index.html";
   }
+    email = localStorage.getItem("user")
+    user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+    user = user.substring(0,user.indexOf("@"))
+    database.ref(user+'/custom').once('value').then((snapshot)=>{
+        current = snapshot.val()
+        if (current == null){
+            current = []
+        }
+        else {
+            current = current.array
+        }
+        sessionStorage.setItem("workout", current)
+    })
 }
 
 function checkIndex(){
