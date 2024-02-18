028831926e0410cb7211b31079c011143bfc2764
fyrebolt <82546290+fyrebolt@users.noreply.github.com>
2024-02-18 08:44:50 -0800

    Update firebaseFunc.js
    suggesting

--- firebaseFunc.js
+++ firebaseFunc.js
@@ -14,6 +14,130 @@ function pushStretchR(id, r2){
     firebase.database().ref(user+'/stretches').push(data)
 }
 
+function suggestedWorkout(){
+    email = localStorage.getItem("user")
+    user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+    user = user.substring(0,user.indexOf("@"))
+
+    const stretchesRef = firebase.database().ref(user+"/stretches");
+    info = []
+    const stretchIds = ['butterfly', 'downwarddog', 'crescent', 'easy', 'triangle', 'reversewarrior', 'tree', 'warrior1', 'warrior2', 'warrior3']
+    
+    stretchesRef.on("value", (snapshot) => {
+            snapshot.forEach(function(data){
+                info.push({id:data.val().id, value:data.val().value})
+            })
+        }
+    );
+
+    for (let i = 0; i < stretchIds.length; i++){
+        temp = true;
+        for (let j = 0; j < info.length; j++){
+            if (info[j].id == stretchIds[i]){
+                temp = false;
+            }
+        }
+        if (temp){
+            info.push({id: stretchIds[i], value: 100})
+        }
+    }
+
+    let highestValueIds = [];
+
+    let max = -1;
+    let ind = 0;
+    for (let i = 0; i < info.length; i++){
+        if (info[i].value > max){
+            ind = i;
+            max = info[i].value
+        }
+    }
+    if (max == 100){
+        completelyRandom = true;
+    }
+    highestValueIds.push(info[ind].id)
+    info.splice(ind, 1);
+    max = -1;
+    ind = 0;
+    for (let i = 0; i < info.length; i++){
+        if (info[i].value > max){
+            ind = i;
+            max = info[i].value
+        }
+    }
+    highestValueIds.push(info[ind].id)
+    info.splice(ind, 1);
+    max = -1;
+    ind = 0;
+    for (let i = 0; i < info.length; i++){
+        if (info[i].value > max){
+            ind = i;
+            max = info[i].value
+        }
+    }
+    highestValueIds.push(info[ind].id)
+    info.splice(ind, 1);
+
+    randomInd = Math.floor(Math.random() * info.length);
+
+    highestValueIds.push(info[randomInd].id)
+    info.splice(randomInd, 1);
+
+    randomInd = Math.floor(Math.random() * info.length);
+
+    highestValueIds.push(info[randomInd].id)
+    info.splice(randomInd, 1);
+
+    if (!completelyRandom){
+        sessionStorage.getItem(highestValueIds);
+        window.location.href="vision.html"
+    }
+    else {
+        stretchesRef.on("value", (snapshot) => {
+            snapshot.forEach(function(data){
+                info.push({id:data.val().id, value:data.val().value})
+                })
+            }
+        );
+    
+        for (let i = 0; i < stretchIds.length; i++){
+            temp = true;
+            for (let j = 0; j < info.length; j++){
+                if (info[j].id == stretchIds[i]){
+                    temp = false;
+                }
+            }
+            if (temp){
+                info.push({id: stretchIds[i], value: 100})
+            }
+        }
+
+        highestValueIds = []
+
+        console.log(info)
+
+        randomInd = Math.floor(Math.random() * info.length);
+        highestValueIds.push(info[randomInd].id)
+        info.splice(randomInd, 1);
+        randomInd = Math.floor(Math.random() * info.length);
+        highestValueIds.push(info[randomInd].id)
+        info.splice(randomInd, 1);
+        randomInd = Math.floor(Math.random() * info.length);
+        highestValueIds.push(info[randomInd].id)
+        info.splice(randomInd, 1);
+        randomInd = Math.floor(Math.random() * info.length);
+        highestValueIds.push(info[randomInd].id)
+        info.splice(randomInd, 1);
+        randomInd = Math.floor(Math.random() * info.length);
+        highestValueIds.push(info[randomInd].id)
+        info.splice(randomInd, 1);
+
+        sessionStorage.getItem(highestValueIds);
+        window.location.href="vision.html"
+    }
+    
+}
+
 function pullStretch(){
     email = localStorage.getItem("user")
     user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
@@ -54,8 +178,6 @@ function pullStretch(){
     }
     highestValueIds.push(info[ind].id)
     info.splice(ind, 1);
-    console.log(highestValueIds)
-
     max = -1;
     ind = 0;
     for (let i = 0; i < info.length; i++){
@@ -66,8 +188,6 @@ function pullStretch(){
     }
     highestValueIds.push(info[ind].id)
     info.splice(ind, 1);
-    console.log(highestValueIds)
-
     max = -1;
     ind = 0;
     for (let i = 0; i < info.length; i++){
@@ -76,10 +196,8 @@ function pullStretch(){
             max = info[i].value
         }
     }
-    
     highestValueIds.push(info[ind].id)
     info.splice(ind, 1);
-    console.log(highestValueIds)
 
     str = "";
     for (let i = 0; i < highestValueIds.length; i++){
