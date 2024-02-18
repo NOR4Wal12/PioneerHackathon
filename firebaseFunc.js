0f382414092de48eaf641248069677925e8f0d2b
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-17 18:34:45 -0800

    s

--- firebaseFunc.js
+++ firebaseFunc.js
@@ -1,3 +1,28 @@
+button = document.getElementById("openSidebar")
+function openNav(){
+    document.getElementById("sidebar").style.width="300px";
+    button.onclick = closeNav
+}
+
+function closeNav(){
+    document.getElementById("sidebar").style.width="0";
+    button.onclick = openNav
+}
+time = document.getElementById("time")
+nameBox = document.getElementById("name")
+name = localStorage.getItem("name")
+nameBox.innerHTML = name
+now = new Date().getHours();
+if (now >= 0 && now <= 11){
+    time.innerHTML = "morning"
+}
+else if (now >=12 && now <= 18){
+    time.innerHTML = "afternoon"
+}
+else {
+    time.innerHTML = "evening"
+}
+
 function pushStretchR(id){
     r2 = document.getElementById("testFirebaseInput").value
     email = localStorage.getItem("user")
