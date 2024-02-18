85e90f1d8c7e3cb1d3aad4c9698b0a59e230e3c9
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2024-02-18 05:24:41 -0800

    s

---
+++ aboutus.js
@@ -0,0 +1,31 @@
+button = document.getElementById("openSidebar")
+function openNav(){
+    document.getElementById("sidebar").style.width="350px";
+    button.onclick = closeNav
+}
+
+function closeNav(){
+    document.getElementById("sidebar").style.width="0";
+    button.onclick = openNav
+}
+time = document.getElementById("time")
+nameBox = document.getElementById("name")
+name = localStorage.getItem("name")
+nameBox.innerHTML = name
+now = new Date().getHours();
+if (now >= 0 && now <= 11){
+    time.innerHTML = "morning"
+}
+else if (now >=12 && now <= 18){
+    time.innerHTML = "afternoon"
+}
+else {
+    time.innerHTML = "evening"
+}
+
+const start = document.getElementById('startStretch')
+start.onclick = (event) =>{
+    event.preventDefault()
+    window.location.href="vision.html"
+    
+}
