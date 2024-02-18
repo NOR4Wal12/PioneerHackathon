b6347102252a934c141aab38cc5e4ed0da5e74fd
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-02-18 00:33:30 -0800

    s

--- profile.js
+++ profile.js
@@ -20,3 +20,32 @@ logoutButton.onclick = () =>{
     localStorage.setItem("user", "null")
     window.location.href="index.html"
 }
+
+function loadList(){
+    if(localStorage.getItem("loggedIn")!="yes"){
+        window.location.href = "index.html";
+    }
+    email = localStorage.getItem("user")
+    user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+    user = user.substring(0,user.indexOf("@"))
+    var str = "";
+    database.ref(user+'/custom').once('value').then((snapshot)=>{
+        current = snapshot.val()
+        if (current == null){
+            current = []
+        }
+        else {
+            current = current.array
+        }
+
+        for (let i = 0; i < current.length; i++){
+            str += (i + 1) + ". " + toDisplay(current[i]) + "</br>";
+        }
+
+        if (str == ""){
+            str = "None currently planned!"
+        }
+
+        document.getElementById("list").innerHTML = str;
+    })
+}
